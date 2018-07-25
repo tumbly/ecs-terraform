a92fe22f1ee67fc8bc1f56404aeccac89d3a0d55
Eric Boismard <eboismard@bcstechno.com>
2018-07-25 18:34:38 +0200

    Nearly working ECS cluster with basic nginx container, one AZ only

--- variables.tf
+++ variables.tf
@@ -1,57 +1,56 @@
-variable "aws_access_key" {
-    description = "The AWS access key."
-}
-
-variable "aws_secret_key" {
-    description = "The AWS secret key."
-}
+#variable "aws_access_key" {
+#    description = "The AWS access key."
+#}
+#
+#variable "aws_secret_key" {
+#    description = "The AWS secret key."
+#}
 
 variable "region" {
-    description = "The AWS region to create resources in."
-    default = "us-east-1"
+  description = "The AWS region to create resources in."
+  default     = "eu-west-3"
 }
 
 # TODO: support multiple availability zones, and default to it.
 variable "availability_zone" {
-    description = "The availability zone"
-    default = "us-east-1a"
+  description = "The availability zone"
+  default     = "eu-west-3a"
 }
 
 variable "ecs_cluster_name" {
-    description = "The name of the Amazon ECS cluster."
-    default = "main"
+  description = "The name of the Amazon ECS cluster."
+  default     = "pnrb-draft"
 }
 
 variable "amis" {
-    description = "Which AMI to spawn. Defaults to the AWS ECS optimized images."
-    # TODO: support other regions.
-    default = {
-        us-east-1 = "ami-ddc7b6b7"
-    }
+  description = "Which AMI to spawn. Defaults to the AWS ECS optimized images."
+
+  default = {
+    # amzn-ami-2018.03.b-amazon-ecs-optimized
+    eu-west-3 = "ami-2187375c"
+  }
 }
 
-
 variable "autoscale_min" {
-    default = "1"
-    description = "Minimum autoscale (number of EC2)"
+  default     = "1"
+  description = "Minimum autoscale (number of EC2)"
 }
 
 variable "autoscale_max" {
-    default = "10"
-    description = "Maximum autoscale (number of EC2)"
+  default     = "3"
+  description = "Maximum autoscale (number of EC2)"
 }
 
 variable "autoscale_desired" {
-    default = "4"
-    description = "Desired autoscale (number of EC2)"
+  default     = "1"
+  description = "Desired autoscale (number of EC2)"
 }
 
-
 variable "instance_type" {
-    default = "t2.micro"
+  default = "t2.micro"
 }
 
 variable "ssh_pubkey_file" {
-    description = "Path to an SSH public key"
-    default = "~/.ssh/id_rsa.pub"
+  description = "Path to an SSH public key"
+  default     = "~/.ssh/id_rsa.pub"
 }
